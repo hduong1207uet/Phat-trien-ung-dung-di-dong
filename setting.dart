a734f7de1197c17b3bde4d50b7075d0b67c7f813
hduong <hduong1207.uet@gmail.com>
2020-10-02 14:24:26 +0700

    Them file settings

--- setting.dart
+++ setting.dart
@@ -16,18 +16,26 @@ class Tflat_Home extends StatelessWidget{
                          child: Text("Cài đặt"),
                     ),
                ),
-               body: Center(
-                    child: Text("Phiên bản 1.27 \nTính năng mới\n"
+               body:
+
+               Center(
+                    child: Text("Phiên bản 1.27 \n\nTính năng mới\n"
                                "*Khắc phục lỗi dữ liệu Offline\n"
                                "*Cập nhật các thư mục từ của bạn\n"
                                "*Cập nhập game học từ vựng\n"
-                               "*Cho phép tạo thư mục cho từ vựng" ,
+                               "*Cho phép tạo thư mục cho từ vựng\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+                               "*Thông báo nhắc nhở học tập"
+                         ,
                      style: TextStyle(fontSize: 18),
                      ),
+
                ),
+
+
+
                floatingActionButton: FloatingActionButton(
                     tooltip: "Thêm câu",
-                    child: Icon(Icons.add),
+                    child: Icon(Icons.alarm_on),
                     onPressed: null,
                ),
           );
