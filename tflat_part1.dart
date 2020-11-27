d580c03fc5b6e6af85fd8a41d17381e3cd6f231e
hduong <hduong1207.uet@gmail.com>
2020-11-27 10:49:33 +0700

    them trang Part 1 hoan thien.

--- tflat_part1.dart
+++ tflat_part1.dart
@@ -1,7 +1,15 @@
+
+import 'dart:io';
+
+import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_toeic_test_tflat/tflat_trang_chu.dart';
-import 'package:flutter_toeic_test_tflat/tflat_kiem_tra_dap_an.dart';
+import 'package:path_provider/path_provider.dart';
+
 
 //Widget
 class tflat_part1 extends StatefulWidget{
@@ -12,12 +20,59 @@ class tflat_part1 extends StatefulWidget{
 //State of Widget
 class _tflat_part1 extends State<tflat_part1> {
 
-  //Dinh nghia cac bien
-   int _groupValue = -1;
-   var map1 = new Map();
-   String dapAn = "";
+//Dinh nghia cac bien
+    int _groupValue = -1;
+    var map1 = new Map();
+    var dapAn = "";
+    var numberOfSentence = 1;
+    int numberOfImage = 1;
 
-  //Khuon mau Radio Button
+    AudioPlayer audioPlayer = new AudioPlayer();
+    Duration duration = new Duration();
+    Duration position = new Duration();
+    bool playing  =  false;
+
+//Thanh chay Audio
+    Widget slider(){
+      return Slider.adaptive(
+          min : 0.0,
+          value: position.inSeconds.toDouble(),
+          max : duration.inSeconds.toDouble(),
+          onChanged: (double value){
+                  setState(() {
+                        audioPlayer.seek(new Duration(seconds: value.toInt()));
+                  });
+          }
+      );
+    }
+
+//Ham chay Local Audio
+    Future<ByteData> loadAsset() async {
+      return await rootBundle.load('assets/audio/part1-${numberOfSentence}.m4a');
+    }
+    void playLocal() async{
+      final file = new File('${(await getTemporaryDirectory()).path}/part1-{$numberOfSentence}.m4a');
+      await file.writeAsBytes((await loadAsset()).buffer.asUint8List());
+      if(!playing){
+        await audioPlayer.pause();
+      }
+      else {
+        await audioPlayer.play(file.path, isLocal: true);
+      }
+      audioPlayer.onDurationChanged.listen((Duration dur) {
+          setState(() {
+            duration =  dur;
+          });
+      });
+      audioPlayer.onAudioPositionChanged.listen((Duration dur) {
+        setState(() {
+            position = dur;
+        });
+      });
+    }
+
+
+//Khuon mau Radio Button
    Widget _myRadioButton({String title, int value, Function onChanged}) {
      return RadioListTile(
        value: value,
@@ -28,31 +83,23 @@ class _tflat_part1 extends State<tflat_part1> {
      );
    }
 
-   void KiemTraDapAn(int valueCheck){
-       var dapAnCauHoi = int.parse(map1["dapAn"]);
-       print(dapAnCauHoi);
+//Ham kiem tra dap an
+   void KiemTraDapAn(int valueCheck) async{
+     map1.clear();
+     DatabaseReference databaseReference = await FirebaseDatabase.instance.reference().child("CauHoi/part1/${numberOfSentence}");
+     databaseReference.once().then((DataSnapshot dataSnapshot) {
        setState(() {
-         switch (dapAnCauHoi){
-           case 1: dapAn = "Đáp án : A"; break;
-           case 2: dapAn = "Đáp án : B"; break;
-           case 3: dapAn = "Đáp án : C"; break;
-           case 4: dapAn = "Đáp án : D";
-         }
+         map1.addAll(dataSnapshot.value);
        });
+     });
    }
 
   @override
   void initState(){
      super.initState();
-     map1.clear();
-     final databaseReference = FirebaseDatabase.instance.reference().child("CauHoi/part1/1");
-     databaseReference.once().then((DataSnapshot dataSnapshot) {
-                setState(() {
-                  map1.addAll(dataSnapshot.value);
-                });
-     });
 
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -66,20 +113,20 @@ class _tflat_part1 extends State<tflat_part1> {
               );
             },
           ),
-          title: Text("Part 1 (1/198)"),
+          title: Text("Part 1 (${numberOfSentence}/198)"),
         ),
 
         body:Column(
           children: <Widget>[
 
             new Image.asset(
-              'assets/questions/part-1/question-1/image-part1-question1.png',
+              'assets/images/part1/1-${numberOfImage}.png',
               width: 300,
               fit: BoxFit.cover,
             ),
 
             _myRadioButton(
-              title: map1["cauA"].toString(),
+              title: map1["cauD"] == null ? "A." : map1["cauA"].toString(),
               value: 1,
               onChanged: (newValue){
                 setState(() {
@@ -90,7 +137,7 @@ class _tflat_part1 extends State<tflat_part1> {
             ),
 
             _myRadioButton(
-              title: map1["cauB"].toString(),
+              title: map1["cauD"] == null ? "B." : map1["cauB"].toString(),
               value: 2,
               onChanged: (newValue){
                 setState(() {
@@ -101,7 +148,7 @@ class _tflat_part1 extends State<tflat_part1> {
             ),
 
             _myRadioButton(
-              title: map1["cauC"].toString(),
+              title: map1["cauD"] == null ? "C." : map1["cauC"].toString(),
               value: 3,
               onChanged: (newValue){
                 setState(() {
@@ -112,7 +159,7 @@ class _tflat_part1 extends State<tflat_part1> {
             ),
 
             _myRadioButton(
-              title: map1["cauD"].toString(),
+              title: map1["cauD"] == null ? "D." : map1["cauD"].toString() ,
               value: 4,
               onChanged: (newValue){
                 setState(() {
@@ -123,7 +170,7 @@ class _tflat_part1 extends State<tflat_part1> {
             ),
 
             Text(
-              dapAn , style: TextStyle(color: Colors.green ,fontSize: 18),
+              map1["dapAn"] == null ? "" : "Đáp án : ${map1["dapAn"].toString()}" , style: TextStyle(color: Colors.green ,fontSize: 18),
             ),
 
           ],
@@ -134,37 +181,46 @@ class _tflat_part1 extends State<tflat_part1> {
         Stack(
             children: [
               Container(
-                height: 70,
+                height: 100,
                 color: Colors.blueAccent,
                 child: Column(
                   children: <Widget>[
                     Container(
-                      height: 20,
-                      color: Colors.blue,
-                      child: Center(child: Text("Thanh chạy audio"),),
+                      height: 30,
+                      color: Colors.white,
+                      child: Center(child: slider(),),
                     ),
                     Container(
-                      height: 50,
+                      height: 70,
                       color: Colors.white,
                       child: Row(
                         children: <Widget>[
 
                           Container(
                             width: 205,
+                            padding: const EdgeInsets.all(12),
                             child: Row(
                               textDirection: TextDirection.ltr,
                               children: [
-                                IconButton(
-                                  onPressed: (){},
-                                  color: Colors.blue,
-                                  icon: Icon(Icons.play_circle_outline),
+                                InkWell(
+                                     child: Icon(
+                                          playing == false
+                                               ? Icons.play_circle_outline
+                                               : Icons.pause_circle_outline,size: 44,
+                                     ),
+                                    onTap: (){
+                                         playLocal();
+                                         setState(() {
+                                             playing = !playing;
+                                         });
+                                    },
                                 ),
                                 FlatButton(
                                   color: Colors.blue,
                                   textColor: Colors.white,
                                   disabledColor: Colors.grey,
                                   disabledTextColor: Colors.black,
-                                  padding: EdgeInsets.all(0.0),
+                                  padding: EdgeInsets.all(10.0),
                                   splashColor: Colors.blueAccent,
                                   onPressed: () {
                                            KiemTraDapAn(_groupValue);
@@ -181,19 +237,42 @@ class _tflat_part1 extends State<tflat_part1> {
 
                           Container(
                             width: 206,
+                              padding: const EdgeInsets.all(10),
                               child: Row(
                                 textDirection: TextDirection.rtl,
                                 children: [
 
                                   IconButton(
-                                    onPressed: (){},
+                                    onPressed: (){
+                                      setState(() {
+                                        map1.clear();
+                                        numberOfSentence ++;
+                                        numberOfImage++ ;
+                                        dapAn = "";
+                                        print(numberOfSentence);
+                                      });
+
+                                    },
                                     color: Colors.blue,
-                                    icon: Icon(Icons.skip_next),
+                                    icon: Icon(Icons.skip_next,size: 30,),
                                   ),
                                   IconButton(
-                                    onPressed: (){},
+                                    onPressed: (){
+                                      setState(() {
+                                        map1.clear();
+                                        if(numberOfSentence > 1){
+                                          numberOfSentence--;
+                                          numberOfImage--;
+                                          dapAn = "";
+                                        }
+                                        else{
+                                        }
+                                        print(numberOfSentence);
+                                      });
+
+                                    },
                                     color: Colors.blue,
-                                    icon: Icon(Icons.skip_previous),
+                                    icon: Icon(Icons.skip_previous,size: 30,),
                                   ),
                                 ],
                               ),
@@ -210,6 +289,8 @@ class _tflat_part1 extends State<tflat_part1> {
 
     );
   }
+
+
 }
 
 
